69e689d72bd3cc1dc47e1fb9318c31a2d4e269e8
abimanyusatriaa <adji3890@gmail.com>
2024-03-14 11:09:11 +0700

    membuat insertionsort

--- 160_Insertionsort/160_Insertionsort.cpp
+++ 160_Insertionsort/160_Insertionsort.cpp
@@ -31,6 +31,34 @@ void input() {                                            //procedure input
     }
 }
 
+void insertionsort() {                                    //procedure insertionsort
+
+    int temp;                                             //membuat variable data temporer atau penyimpanan sementara
+    int i, j;                                             //membuat variable j sebagai penanda
+
+    for (i = 1; i <= n - 1; i++) {                        //1. looping dengan 1 ddimulai dari 1 hingga n-1
+
+        temp = arr[i];                                    //2. simpan nilai arr[1] ke variable sementara temp
+
+        j = i - 1;                                        //3. setting nilai j sama dengan i-1
+
+        while (j >= 0 && arr[j] > temp)                   //4. looping while dimana niali j lebih besar sama dengan 0 dan
+            //arr[j] lebih besar daripada temp
+        {
+            arr[j + 1] = arr[j];                          //4a. simpan arr[j] ke dalam variable arr[j+1]
+            j--;                                          //4b. decrement nilai j by 1
+        }
+
+        arr[j + 1] = temp;                                //5. simpan nilai temp ke dalam arr[j+1]
+
+        cout << "\nPass" << i << ": ";                    //output ke layar
+        for (int k = 0; k < n; k++) {                     //looping nilai k dimulai dari 0 hingga n-1
+            cout << arr[k] << " ";                        //output ke layar
+        }
+
+    }
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
